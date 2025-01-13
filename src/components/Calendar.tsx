7c4706ab539cce8f0e16fc0981ba93e3e674542d
Yves Riedener <git@reecube.com>
2025-01-13 14:11:19 +0100

    WIP

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,5 +1,4 @@
 import React, { ReactChild } from 'react';
-import { TopPartOfCalendar } from './TopPartOfCalendar';
 import {
   getCachedDateTimeFormat,
   getDaysInMonth,
@@ -391,4 +390,43 @@ type CalendarProps = {
  readonly columnWidth: number;
  readonly fontFamily: string;
  readonly fontSize: string;
+};
+
+const TopPartOfCalendar: React.FC<TopPartOfCalendarProps> = ({
+                                                               value,
+                                                               x1Line,
+                                                               y1Line,
+                                                               y2Line,
+                                                               xText,
+                                                               yText,
+                                                             }) => {
+  return (
+    <g>
+      <line
+        x1={x1Line}
+        y1={y1Line}
+        x2={x1Line}
+        y2={y2Line}
+        className="stroke-gray-200"
+        key={value + 'line'}
+      />
+      <text
+        key={value + 'text'}
+        y={yText}
+        x={xText}
+        className="stroke-gray-200"
+      >
+        {value}
+      </text>
+    </g>
+  );
+};
+
+type TopPartOfCalendarProps = {
+  readonly value: string;
+  readonly x1Line: number;
+  readonly y1Line: number;
+  readonly y2Line: number;
+  readonly xText: number;
+  readonly yText: number;
 };
