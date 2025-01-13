eccce088b3a1be967309d7b6ecb6ad3c5e0e3379
Yves Riedener <git@reecube.com>
2025-01-13 17:11:10 +0100

    Fixes

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,9 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import { builtinModules } from 'module';
-import * as packageJson from './package.json' with { type: 'json' };
+import * as fs from 'fs';
+
+const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 
 export default {
   input: 'src/index.ts',
